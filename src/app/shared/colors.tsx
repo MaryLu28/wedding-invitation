76be6d277f0fced13bc470e4b7ee12700d8e3e3b
María Lourdes <marylo281292@gmail.com>
2024-03-21 04:31:31 -0300

    Add colors

--- src/app/shared/colors.tsx
+++ src/app/shared/colors.tsx
@@ -1,3 +1,31 @@
+import { ColorFormat } from "react-countdown-circle-timer";
+
 export const colors = {
-  text: "#524A3F",
+  darkerYellow: "#524A3F",
+
+  yellow1: "#966625",
+  yellow2: "#A27432",
+  yellow3: "#AC8243",
+  yellow4: "#BA9457",
+  yellow5: "#C29F66",
+  yellow6: "#CAA971",
+  yellow7: "#D6BB8F",
+
+  brightYellow1: "#E3BF8E",
+  brightYellow2: "#E0C49B",
+  brightYellow4: "#E1D0B6",
+  brightYellow6: "#E8DAC1",
+  brightYellow7: "#EDE1CE",
+
+  color1: "#DCC8B4",
+  color2: "#ECDDCA",
+  color3: "#FFECCC",
+
+  white1: "#F8F7F6",
+
+  gray1: "#F3F3F3",
+  gray2: "#DFDFDF",
+  gray3: "#CDCDCD",
+  gray4: "#BCBCBC",
+  gray5: "#AAAAAA",
 };
